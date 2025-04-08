187d6899b7bfaef240d536e436e06aa551f39942
Zhang Yichen <z1181806270@gmail.com>
2025-04-09 00:40:57 +0800

    移除 uni_modules 和 unpackage 目录的 Git 追踪

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const _imports_0$1 = "/static/images/user.png";
-const _imports_0 = "/static/images/no-data.png";
-exports._imports_0 = _imports_0$1;
-exports._imports_0$1 = _imports_0;
